66bc215028c6a01fa6a8dde8ca9c71821cf1f60b
mjacquot <marcel.m.jacquot@gmail.com>
2023-10-04 20:49:18 -0700

    added base env variables

--- core/wsgi.py
+++ core/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from decouple import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+# DJANGO_SETTINGS_MODULE_CONFIG is an env variable imported through decouple
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE_CONFIG")) 
 
 application = get_wsgi_application()
