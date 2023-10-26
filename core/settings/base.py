5a224ece4763986c26a02c67b3a4807750d61466
mjacquot <marcel.m.jacquot@gmail.com>
2023-10-25 17:27:46 -0700

    created base bootstrap template, commented out default urls, added ui_testing app

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,7 +16,8 @@ import string
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# three parents makes it go from 'settings' to 'core' to 'personal_website'
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "home",
+    "ui_testing",
+    "bootstrap5",
     'theme_soft_design',
 ]
 
