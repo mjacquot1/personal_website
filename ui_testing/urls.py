801bc15ee45d347f1d64aec0ea63d02d585eec2d
mjacquot <marcel.m.jacquot@gmail.com>
2023-11-18 15:53:12 -0800

    added resum icons. Split resume into its own url

--- ui_testing/urls.py
+++ ui_testing/urls.py
@@ -7,7 +7,6 @@ from . import views
 app_name = 'ui_testing'
 
 urlpatterns = [
-    path('', views.test_404, name='test_404'),
-    path('demo', views.demo, name='demo'),
-    # path('ignore_again/', views.test_404, name='ignore'),
+    path('resume/', views.test_404, name='test_404'),
+    path('demo/', views.demo, name='demo'),
 ]
